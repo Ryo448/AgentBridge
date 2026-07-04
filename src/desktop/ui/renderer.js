d5bda5533d6be08b934e9becb9f341decff61c22
Ryo448 <maryabbdd@gmail.com>
2026-07-04 20:18:53 -0300

    Bump version to 4.2.4 in terminal and desktop client

--- src/desktop/ui/renderer.js
+++ src/desktop/ui/renderer.js
@@ -20,7 +20,7 @@ const previewStatus = {
     ? window.agentBridgeModels.map((item) => item.model)
     : [],
   provider: 'NVIDIA',
-  appVersion: '4.2.3',
+  appVersion: '4.2.4',
   apiKey: 'EuAmoORyo',
   needLocalKey: false,
   codexBaseUrl: 'http://localhost:3000/v1',
